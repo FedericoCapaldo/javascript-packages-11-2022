0b3eb59d18a40570f3da148a2bc2ac77bbcf3922
Federico <federico.capaldo@hotmail.com>
2022-11-09 21:46:25 +0000

    refactored sortable js out in its stimulus controller, removed unnecessary code from index.js, removed unnecessary plugins folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-import { initSortable } from "./plugins/init_sortable"
-
-const list = document.querySelector("#results")
 
 window.Stimulus = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
-
-
-initSortable()
