78f2df16121626e33019dc2ce7cb9ed1b705cf06
Federico <federico.capaldo@hotmail.com>
2022-11-09 21:43:29 +0000

    refactored movies into its own stimulus controller

--- src/index.js
+++ src/index.js
@@ -9,30 +9,4 @@ const context = require.context("./controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
 
 
-const insertMovies = (data) => {
-  data.Search.forEach((result) => {
-    const movieTag = `<li class="list-group-item border-0">
-      <img src="${result.Poster}" alt="" width="100">
-    </li>`
-    list.insertAdjacentHTML("beforeend", movieTag)
-  })
-}
-
-const fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-    .then(response => response.json())
-    .then(data => insertMovies(data))
-}
-
-fetchMovies("harry potter") // on 1st page load
-
-const form = document.querySelector("#search-form")
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  list.innerHTML = ""
-  const input = document.querySelector("#search-input")
-  fetchMovies(input.value)
-})
-
-
 initSortable()
