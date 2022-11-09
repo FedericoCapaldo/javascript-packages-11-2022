52c6b6c84fd0464948ccd2516b272be12a480441
Federico <federico.capaldo@hotmail.com>
2022-11-09 21:12:35 +0000

    setup stimulus and wired up with console.log the first stimulus controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 import { initSortable } from "./plugins/init_sortable"
 
 const list = document.querySelector("#results")
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+
 
 const insertMovies = (data) => {
   data.Search.forEach((result) => {
